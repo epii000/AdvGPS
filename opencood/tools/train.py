5fae9f37f694fc8674e014cddb03a0f3de7108f8
hantang <1264556381@qq.com>
2025-10-15 17:02:18 +0800

    feat: 修改相关路径

--- opencood/tools/train.py
+++ opencood/tools/train.py
@@ -4,7 +4,7 @@ import statistics
 
 import torch
 import tqdm
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, DistributedSampler
 from tensorboardX import SummaryWriter
 
 
@@ -24,19 +24,22 @@ print(sys.path)
 
 import opencood.hypes_yaml.yaml_utils as yaml_utils
 from opencood.tools import train_utils
+from opencood.tools import multi_gpu_utils
 from opencood.data_utils.datasets import build_dataset
 
 
 
 def train_parser():
     parser = argparse.ArgumentParser(description="synthetic data generation")
-    parser.add_argument("--hypes_yaml", type=str,default="/home/jinlongli/1.Detection_Set/V2V_Attack/opencood/hypes_yaml/point_pillar_intermediate_V2VAM.yaml",#
+    parser.add_argument("--hypes_yaml", type=str,default="/home/miajun/hantang/AdvGPS/opencood/hypes_yaml/point_pillar_intermediate_V2VAM.yaml",#
                         help='data generation yaml file needed ')
     parser.add_argument('--model_dir', default="", #home/jinlongli/2.model_saved/3.Attack_V2V2023/0.attack_model/V2VAM_finetune_voxel_2023_07_21_07
                         help='Continued training path')
     parser.add_argument('--model', default='',
                         help='for fine-tuned training path')
     parser.add_argument("--half", action='store_true', help="whether train with half precision")
+    parser.add_argument('--dist_url', default='env://',
+                        help='url used to set up distributed training')
     opt = parser.parse_args()
     return opt
 
@@ -45,46 +48,52 @@ def main():
     opt = train_parser()
     hypes = yaml_utils.load_yaml(opt.hypes_yaml, opt)
 
+    multi_gpu_utils.init_distributed_mode(opt)
+
     print('Dataset Building')
     opencood_train_dataset = build_dataset(hypes, visualize=False, train=True,isSim=True)
     opencood_validate_dataset = build_dataset(hypes,
                                               visualize=False,
                                               train=False,
                                               isSim=True)
+    if opt.distributed:
+        sampler_train = DistributedSampler(opencood_train_dataset)
+        sampler_val = DistributedSampler(opencood_validate_dataset,
+                                         shuffle=False)
 
-    train_loader = DataLoader(opencood_train_dataset,
-                              batch_size=hypes['train_params']['batch_size'],
-                              num_workers=8,
-                              collate_fn=opencood_train_dataset.collate_batch_train,
-                              shuffle=True,
-                              pin_memory=False,
-                              drop_last=True)
-    val_loader = DataLoader(opencood_validate_dataset,
-                            batch_size=hypes['train_params']['batch_size'],
-                            num_workers=8,
-                            collate_fn=opencood_train_dataset.collate_batch_train,
-                            shuffle=False,
-                            pin_memory=False,
-                            drop_last=True)
+        batch_sampler_train = torch.utils.data.BatchSampler(
+            sampler_train, hypes['train_params']['batch_size'], drop_last=True)
+
+        train_loader = DataLoader(opencood_train_dataset,
+                                  batch_sampler=batch_sampler_train,
+                                  num_workers=8,
+                                  collate_fn=opencood_train_dataset.collate_batch_train)
+        val_loader = DataLoader(opencood_validate_dataset,
+                                sampler=sampler_val,
+                                num_workers=8,
+                                collate_fn=opencood_train_dataset.collate_batch_train,
+                                drop_last=False)
+    else:
+        train_loader = DataLoader(opencood_train_dataset,
+                                batch_size=hypes['train_params']['batch_size'],
+                                num_workers=8,
+                                collate_fn=opencood_train_dataset.collate_batch_train,
+                                shuffle=True,
+                                pin_memory=False,
+                                drop_last=True)
+        val_loader = DataLoader(opencood_validate_dataset,
+                                batch_size=hypes['train_params']['batch_size'],
+                                num_workers=8,
+                                collate_fn=opencood_train_dataset.collate_batch_train,
+                                shuffle=False,
+                                pin_memory=False,
+                                drop_last=True)
 
     print('Creating Model')
     model = train_utils.create_model(hypes)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # we assume gpu is necessary
-    if torch.cuda.is_available():
-        model.to(device)
-
-    # define the loss
-    criterion = train_utils.create_loss(hypes)
-
-    # optimizer setup
-    optimizer = train_utils.setup_optimizer(hypes, model)
-    # lr scheduler setup
-    num_steps = len(train_loader)
-    scheduler = train_utils.setup_lr_schedular(hypes, optimizer, num_steps)
-
-   # if we want to train from last checkpoint.
+    # if we want to train from last checkpoint.
     if opt.model_dir:
         saved_path = opt.model_dir
         init_epoch, model = train_utils.load_saved_model(saved_path, model)
@@ -108,6 +117,27 @@ def main():
             # to save the model,
             saved_path = train_utils.setup_train(hypes)
 
+    # we assume gpu is necessary
+    if torch.cuda.is_available():
+        model.to(device)
+    model_without_ddp = model
+
+    if opt.distributed:
+        model = \
+            torch.nn.parallel.DistributedDataParallel(model,
+                                                      device_ids=[opt.gpu],
+                                                      find_unused_parameters=True)
+        model_without_ddp = model.module   
+
+    # define the loss
+    criterion = train_utils.create_loss(hypes)
+
+    # optimizer setup
+    optimizer = train_utils.setup_optimizer(hypes, model_without_ddp)
+    # lr scheduler setup
+    num_steps = len(train_loader)
+    scheduler = train_utils.setup_lr_schedular(hypes, optimizer, num_steps)
+
     # record training
     writer = SummaryWriter(saved_path)
 
@@ -124,8 +154,14 @@ def main():
     for epoch in range(init_epoch, max(epoches, init_epoch)):
         if hypes['lr_scheduler']['core_method'] != 'cosineannealwarm':
             scheduler.step(epoch)
+        if hypes['lr_scheduler']['core_method'] == 'cosineannealwarm':
+            scheduler.step_update(epoch * num_steps + 0)
         for param_group in optimizer.param_groups:
             print('learning rate %f' % param_group["lr"])
+
+        if opt.distributed:
+            sampler_train.set_epoch(epoch)
+
         pbar2 = tqdm.tqdm(total=len(train_loader), leave=True)
         for i, batch_data in enumerate(train_loader):
             # the model will be evaluation mode during validation
